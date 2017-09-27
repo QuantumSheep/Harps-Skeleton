72f05daf211d07c97150028a4a3b5030c18ee8a4
QuantumSheep <quantumsheep@outlook.fr>
2017-09-27 22:15:07 +0200

    Updated some lines to match W3C markup rules

--- app/Views/default.blade.php
+++ app/Views/default.blade.php
@@ -1,7 +1,9 @@
 @extends('/templates/_Layout')
 @section('title', 'Welcome to Harps')
 @section('content')
-Welcome to Harps!<br />
-Your php version is {{ $model->php_version }}<br />
-The current uri is {{ $model->current_uri }}
+<section>
+    Welcome to Harps!<br />
+    Your php version is {{ $model->php_version }}<br />
+    The current uri is {{ $model->current_uri }}
+</section>
 @endsection
